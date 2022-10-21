78083b71af7985347f2027b3600a47b7af6e1881
Carolina Chung <chechung@umich.edu>
2022-10-21 16:03:36 -0400

    Updating documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./..'))
-
+# sys.path.insert(0, os.path.abspath('./..'))
+sys.path.insert(0, os.path.abspath('..'))
+# sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 # -- Project information -----------------------------------------------------
 
