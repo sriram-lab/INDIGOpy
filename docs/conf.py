4f949fdf58f97bd835afd81ae9ccefbe7ec867ac
Carolina Chung <chechung@umich.edu>
2022-10-22 02:44:57 -0400

    Updating docs with updated conf.py and requirements.txt.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,11 @@ author = 'sriram-lab'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = [
+	'sphinx.ext.autodoc', 
+	'sphinx.ext.napoleon', 
+	'sphinx.ext.viewcode', 
+	'sphinx.ext.coverage',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'press'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
